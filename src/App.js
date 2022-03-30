6e97cb003b49ab0cb51a09d9d5837b6889d3e45e
Christian von Lehe <christian.lehe@storytel.com>
2022-03-30 15:09:21 -0400

    chore: add in code comments

--- src/App.js
+++ src/App.js
@@ -1,58 +1,78 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import { addWatchedMovie, add, removeWatchedMovie, getWatchedMovies, getAllMovies } from './index.js';
+import {
+  addWatchedMovie,
+  add,
+  removeWatchedMovie,
+  getWatchedMovies,
+  getAllMovies,
+} from "./index.js";
 
+// better to separate this into it's own component
 const getMoviesComponents = (movies) => {
   var components = [];
 
-  movies.forEach(function(movie) {
+  // .map() is a better approach to avoid having to create the components array and pushing to it
+  movies.forEach(function (movie) {
     components.push(
       <div className="all">
         <div>
           <img src={movie.image} height="100px" />
         </div>
         <span>
-          <a className="movie-watched" href="#" onClick={function() { addWatchedMovie(movie.title, movie.comment, movie.image) }}>
+          <a
+            className="movie-watched"
+            href="#"
+            onClick={function () {
+              addWatchedMovie(movie.title, movie.comment, movie.image);
+            }}
+          >
             {movie.title}
           </a>
         </span>
         <br />
-        <span>
-          {movie.comment}
-        </span>
+        <span>{movie.comment}</span>
         <br />
         <br />
       </div>
-    )
-  })
+    );
+  });
 
   return components;
-}
+};
 
+// can combine this with the one above with some logic
 function getWatchedMoviesComponents(movies) {
   var components = [];
 
-  movies.forEach(function(movie) {
-    components.push(movie && (
-      <div className="watched">
-        <div>
-          <img src={movie.image} height="100px" />
+  // .map() is a better approach to avoid having to create the components array and pushing to it
+  movies.forEach(function (movie) {
+    components.push(
+      movie && (
+        <div className="watched">
+          <div>
+            <img src={movie.image} height="100px" />
+          </div>
+          <span>
+            <a
+              className="movie-watched"
+              href="#"
+              onClick={function () {
+                removeWatchedMovie(movie.title);
+              }}
+            >
+              {movie.title}
+            </a>
+          </span>
+          <br />
+          <span>{movie.comment}</span>
+          <br />
+          <br />
         </div>
-        <span>
-          <a className="movie-watched" href="#" onClick={function() { removeWatchedMovie(movie.title) }}>
-            {movie.title}
-          </a>
-        </span>
-        <br />
-        <span>
-          {movie.comment}
-        </span>
-        <br />
-        <br />
-      </div>
-    ))
-  })
+      )
+    );
+  });
 
   return components;
 }
@@ -62,22 +82,61 @@ function App(props) {
     <div className="App">
       <h1>Codest Movies!</h1>
       <h1>Add movie!</h1>
-      <b>TITLE:<br /><input type="text" onChange={function(e) { title = e.target.value; }} /></b><br />
-      <b>IMAGE URL:<br /><input type="text" onChange={function(e) { image = e.target.value; }} /></b><br />
-      <b>COMMENT:<br /><input type="text" onChange={function(e) { comment = e.target.value; }} /></b><br />
-      <input type="button" onClick={function(e) { add(title, image, comment); }} value="ADD MOVIE" />
+      <b>
+        TITLE:
+        <br />
+        <input
+          type="text"
+          onChange={function (e) {
+            title = e.target.value;
+          }}
+        />
+      </b>
+      <br />
+      <b>
+        IMAGE URL:
+        <br />
+        <input
+          type="text"
+          onChange={function (e) {
+            image = e.target.value;
+          }}
+        />
+      </b>
+      <br />
+      <b>
+        COMMENT:
+        <br />
+        <input
+          type="text"
+          onChange={function (e) {
+            comment = e.target.value;
+          }}
+        />
+      </b>
+      <br />
+      <input
+        type="button"
+        onClick={function (e) {
+          add(title, image, comment);
+        }}
+        value="ADD MOVIE"
+      />
 
       <h1>Watchlist:</h1>
+      {/* would be cleaner to then use the components I spoke of above */}
       {getMoviesComponents(getAllMovies())}
 
       <h1>Already watched:</h1>
+      {/* would be cleaner to then use the components I spoke of above */}
       {getWatchedMoviesComponents(getWatchedMovies())}
     </div>
   );
 }
 
-var title = '';
-var image = '';
-var comment = '';
+// put these values in our state
+var title = "";
+var image = "";
+var comment = "";
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,89 +1,98 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
+// All of these functions can be moved into their own actions file to be a bit more readable, no need to have them in the same file as our index page
 export function getWatchedMovies() {
-	var movies = localStorage.getItem('movies-watched');
+  var movies = localStorage.getItem("movies-watched");
 
-	if (!movies) {
-		return [];
-	} else {
-		return JSON.parse(movies);
-	}
+  if (!movies) {
+    return [];
+    // personal prefrence, but else statements aren't needed here
+  } else {
+    return JSON.parse(movies);
+  }
 }
 
 export function getAllMovies() {
-	var movies = localStorage.getItem('movies-all');
+  var movies = localStorage.getItem("movies-all");
 
-	if (!movies) {
-		return [
-		{
-			title: 'The Avengers',
-			image: 'http://d21lz9b0v8r1zn.cloudfront.net/wp-content/uploads//2012/03/detail.jpg',
-			comment: 'New York blows up in this!'
-		},
-		{
-			title: 'Dark City',
-			image: 'https://i.chzbgr.com/full/5569379584/hA96709E0/',
-			comment: 'This looks mysterious. Cool!'
-		},
-		{
-			title: 'Hot Tub Time Machine',
-			image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG7vNmphIcVhEcybvSvMgbTkV6EE2twHBNanKvgDx3ZS7Ivn6Dtg',
-			comment: 'Someone said this was fun. Maybe!'
-		},
-		];
-	} else {
-		return JSON.parse(movies);
-	}
+  if (!movies) {
+    return [
+      {
+        title: "The Avengers",
+        image:
+          "http://d21lz9b0v8r1zn.cloudfront.net/wp-content/uploads//2012/03/detail.jpg",
+        comment: "New York blows up in this!",
+      },
+      {
+        title: "Dark City",
+        image: "https://i.chzbgr.com/full/5569379584/hA96709E0/",
+        comment: "This looks mysterious. Cool!",
+      },
+      {
+        title: "Hot Tub Time Machine",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG7vNmphIcVhEcybvSvMgbTkV6EE2twHBNanKvgDx3ZS7Ivn6Dtg",
+        comment: "Someone said this was fun. Maybe!",
+      },
+    ];
+    // personal prefrence, but else statements aren't needed here
+  } else {
+    return JSON.parse(movies);
+  }
 }
 
 export function add(title, description, image) {
-	var movie = {};
-	movie.title = title;
-	movie.description = description;
-	movie.image = image;
+  var movie = {};
+  movie.title = title;
+  movie.description = description;
+  movie.image = image;
 
-	var movies = getAllMovies();
-	movies.push(movie);
+  var movies = getAllMovies();
+  movies.push(movie);
 
-	localStorage.setItem('movies-all', JSON.stringify(movies));
+  localStorage.setItem("movies-all", JSON.stringify(movies));
 
-	render();
+  render();
 }
 
 export function addWatchedMovie(title, description, image) {
-	var movie = {};
-	movie.title = title;
-	movie.description = description;
-	movie.image = image;
+  var movie = {};
+  movie.title = title;
+  movie.description = description;
+  movie.image = image;
 
-	var movies = getWatchedMovies();
-	movies.push(movie);
+  var movies = getWatchedMovies();
+  movies.push(movie);
 
-	localStorage.setItem('movies-watched', JSON.stringify(movies));
+  localStorage.setItem("movies-watched", JSON.stringify(movies));
 
-	render();
+  render();
 }
 
 export function removeWatchedMovie(title) {
-	var movies = getWatchedMovies();
+  var movies = getWatchedMovies();
 
-	for (var i = 0; i < movies.length; i++) {
-	   if (!movies[i]) continue;
-		if (movies[i].title == title) {
-			movies[i] = null
-		}
-	}
+  for (var i = 0; i < movies.length; i++) {
+    if (!movies[i]) continue;
+    if (movies[i].title == title) {
+      movies[i] = null;
+    }
+  }
 
-	localStorage.setItem('movies-watched', JSON.stringify(movies));
+  localStorage.setItem("movies-watched", JSON.stringify(movies));
 
-	render();
+  render();
 }
 
+// not needed to make this a function, can call ReactDOM.render directly
 function render() {
-	ReactDOM.render(<App movies={getAllMovies()} watched={getWatchedMovies()} />, document.getElementById('root'))
+  ReactDOM.render(
+    <App movies={getAllMovies()} watched={getWatchedMovies()} />,
+    document.getElementById("root")
+  );
 }
 
 render();
